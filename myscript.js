44f91fd6ca6f71f017005e300c04b74242a11aeb
Your Name <you@example.com>
2025-02-06 13:38:32 +0530

    jmghnfn

--- myscript.js
+++ myscript.js
@@ -1,10 +1,18 @@
 function f1(){
-    let e=document.getElementById("email").value
-    let p=document.getElementById("pass").value
-    if(e!="john@gmail.com" || p!="1234"){
-        message.innerHTML = "denied";
-    }
-    else{
-        message.innerHTML = "noice";
-    }
+    let n=document.getElementById("name").value;
+    let e=document.getElementById("email").value;
+    let p=document.getElementById("pass").value;
+    let d=document.getElementById("date").value;
+    let message = document.getElementById("message");
+    // if(e!="john@gmail.com" || p!="1234"){
+    //     message.innerHTML = "denied";
+    // }
+    // else if(e==NULL || p==NULL){
+    //     message.innerHTML="likhde kuch"
+    // }
+    // else{
+    //     message.innerHTML = "noice";
+    // }
+    message.innerHTML += `${n},${e},${p},${d}<br>`;
+    //document.write(e+" "+p);
 }
